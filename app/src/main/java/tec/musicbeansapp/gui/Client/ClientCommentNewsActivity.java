76766c89e09b885f1efd00daf437f5b086607d75
rshum <ricardoshumestrada@gmail.com>
2018-09-19 10:27:18 -0600

    [fixed] Comment news activity for client

--- app/src/main/java/tec/musicbeansapp/gui/Client/ClientCommentNewsActivity.java
+++ app/src/main/java/tec/musicbeansapp/gui/Client/ClientCommentNewsActivity.java
@@ -3,17 +3,31 @@ package tec.musicbeansapp.gui.Client;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import tec.musicbeansapp.R;
+import tec.musicbeansapp.gui.Admin.Accounts.CreateBandActivity;
 
 public class ClientCommentNewsActivity extends AppCompatActivity {
+
+    // Vars
+    int newID = 0; //FIXME: Dummy ID value
+
+    // UI Views
+    EditText textClientComment;
+    Button btnClientCommentNews;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client_comment_news);
+
+        textClientComment = (EditText) findViewById(R.id.textClientComment);
+        btnClientCommentNews = (Button) findViewById(R.id.btnClientCommentNews);
 
         TextView toolBarText = (TextView) findViewById(R.id.txtToolbarText);
         toolBarText.setText("Admin");
@@ -23,6 +37,15 @@ public class ClientCommentNewsActivity extends AppCompatActivity {
             public void onClick(View view) {
                 System.out.println("LOG: Navigating back to Log in Activity");
                 finish();
+            }
+        });
+
+        btnClientCommentNews.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("LOG: Trying to post a comment on news: " + newID );
+                Toast.makeText(ClientCommentNewsActivity.this, "Comment posted",
+                        Toast.LENGTH_SHORT).show();
             }
         });
     }
